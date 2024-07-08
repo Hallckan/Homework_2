30b0fefae045e4a06be0dfb8a403e839d3f88976
Hallckan <hallckan@gmail.com>
2024-07-09 01:06:17 +0400

    init commit

---
+++ app/src/main/java/com/example/homework_2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.homework_2
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button_to_activity_2)
+
+        button.setOnClickListener(){
+            setContentView(R.layout.activity_2)
+            Toast.makeText(this, R.string.string_hello_world, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
